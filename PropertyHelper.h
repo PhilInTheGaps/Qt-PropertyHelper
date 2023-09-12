5bf090b9ffb607fab494d9b8d6ea599d952ed2a0
Phil Hoffmann <philhoffmann1337@gmail.com>
2023-09-12 20:39:01 +0200

    Added additional macros for qml list properties

--- PropertyHelper.h
+++ PropertyHelper.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QObject>
+#include <QQmlListProperty>
 
 #define AUTO_PROPERTY(TYPE, NAME)                                                                                      \
     Q_PROPERTY(TYPE NAME READ NAME WRITE NAME NOTIFY NAME##Changed)                                                    \
@@ -74,6 +75,28 @@ public:                                                                         
 private:                                                                                                               \
     TYPE a_##NAME = DEFAULT;
 
+#define AUTO_LIST_PROPERTY(TYPE, NAME)                                                                                 \
+    Q_PROPERTY(QQmlListProperty<TYPE> NAME READ NAME##Qml NOTIFY NAME##Changed)                                        \
+public:                                                                                                                \
+    [[nodiscard]] auto NAME() const->QList<TYPE *>                                                                     \
+    {                                                                                                                  \
+        return a_##NAME;                                                                                               \
+    }                                                                                                                  \
+    [[nodiscard]] auto NAME##Qml()->QQmlListProperty<TYPE>                                                             \
+    {                                                                                                                  \
+        return QQmlListProperty(this, &a_##NAME);                                                                      \
+    }                                                                                                                  \
+    void NAME(const QList<TYPE *> &value)                                                                              \
+    {                                                                                                                  \
+        if (a_##NAME == value) return;                                                                                 \
+        a_##NAME = value;                                                                                              \
+        emit NAME##Changed(value);                                                                                     \
+    }                                                                                                                  \
+    Q_SIGNAL void NAME##Changed(QList<TYPE *> value);                                                                  \
+                                                                                                                       \
+private:                                                                                                               \
+    QList<TYPE *> a_##NAME;
+
 #define READONLY_PROPERTY(TYPE, NAME)                                                                                  \
     Q_PROPERTY(TYPE NAME READ NAME CONSTANT)                                                                           \
 public:                                                                                                                \
@@ -119,3 +142,19 @@ public:                                                                         
                                                                                                                        \
 private:                                                                                                               \
     TYPE a_##NAME = DEFAULT;
+
+#define READ_LIST_PROPERTY(TYPE, NAME)                                                                                 \
+    Q_PROPERTY(QQmlListProperty<TYPE> NAME READ NAME##Qml NOTIFY NAME##Changed)                                        \
+public:                                                                                                                \
+    [[nodiscard]] auto NAME() const->QList<TYPE *>                                                                     \
+    {                                                                                                                  \
+        return a_##NAME;                                                                                               \
+    }                                                                                                                  \
+    [[nodiscard]] auto NAME##Qml()->QQmlListProperty<TYPE>                                                             \
+    {                                                                                                                  \
+        return QQmlListProperty(this, &a_##NAME);                                                                      \
+    }                                                                                                                  \
+    Q_SIGNAL void NAME##Changed();                                                                                     \
+                                                                                                                       \
+private:                                                                                                               \
+    QList<TYPE *> a_##NAME;
